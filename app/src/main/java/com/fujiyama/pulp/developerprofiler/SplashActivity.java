36acc6d983e57f9a2a023051c8aa1045f9b6571c
Eagan Martin <mece.martinece@gmail.com>
2018-10-04 15:44:44 +0800

    Added blurring of background for splashscreen;

--- app/src/main/java/com/fujiyama/pulp/developerprofiler/SplashActivity.java
+++ app/src/main/java/com/fujiyama/pulp/developerprofiler/SplashActivity.java
@@ -1,7 +1,13 @@
 package com.fujiyama.pulp.developerprofiler;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.fujiyama.pulp.developerprofiler.utilities.ImageHandler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +15,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.splashbackground);
+        Bitmap blurredBitmap = ImageHandler.blur(this, originalBitmap);
+
+        LinearLayout splashscreenView = (LinearLayout) findViewById(R.id.splashscreen);
+        splashscreenView.setBackground(new BitmapDrawable(getResources(), blurredBitmap));
     }
 }
--- app/src/main/java/com/fujiyama/pulp/developerprofiler/utilities/ImageHandler.java
+++ app/src/main/java/com/fujiyama/pulp/developerprofiler/utilities/ImageHandler.java
@@ -8,7 +8,7 @@ import android.renderscript.RenderScript;
 import android.renderscript.ScriptIntrinsicBlur;
 
 public class ImageHandler {
-    private static final float BITMAP_SCALE = 0.4f;
+    private static final float BITMAP_SCALE = 0.2f;
     private static final float BLUR_RADIUS = 7.5f;
 
     public static Bitmap blur(Context context, Bitmap image) {
