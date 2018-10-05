526bb518ced30b0cbd3db9adae87ed3eba42a2a8
Eagan Martin <mece.martinece@gmail.com>
2018-10-05 15:42:47 +0800

    Added resource for animations;

--- app/src/main/java/com/fujiyama/pulp/developerprofiler/SplashActivity.java
+++ app/src/main/java/com/fujiyama/pulp/developerprofiler/SplashActivity.java
@@ -5,11 +5,18 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.fujiyama.pulp.developerprofiler.utilities.ImageHandler;
 
 public class SplashActivity extends AppCompatActivity {
+
+    LinearLayout splashScreen, splashHeader, splashBody;
+    EditText githubUserField;
+    Button viewProfileButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +26,13 @@ public class SplashActivity extends AppCompatActivity {
         Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.splash_background);
         Bitmap blurredBitmap = ImageHandler.blur(this, originalBitmap);
 
-        LinearLayout splashscreenView = (LinearLayout) findViewById(R.id.splashscreen);
-        splashscreenView.setBackground(new BitmapDrawable(getResources(), blurredBitmap));
+        splashScreen = (LinearLayout) findViewById(R.id.splashScreen);
+        splashScreen.setBackground(new BitmapDrawable(getResources(), blurredBitmap));
+
+        splashHeader = (LinearLayout) findViewById(R.id.splashHeader);
+        splashBody = (LinearLayout) findViewById(R.id.splashBody);
+
+        githubUserField = (EditText) findViewById(R.id.githubUserField);
+        viewProfileButton = (Button) findViewById(R.id.viewProfileButton);
     }
 }
