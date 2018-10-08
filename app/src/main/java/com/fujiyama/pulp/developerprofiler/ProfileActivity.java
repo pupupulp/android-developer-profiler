4e29c718a7bf7267da1d39effe2505c6aec15bc4
Eagan Martin <mece.martinece@gmail.com>
2018-10-08 21:10:32 +0800

    Added displaying of other profile details;

--- app/src/main/java/com/fujiyama/pulp/developerprofiler/ProfileActivity.java
+++ app/src/main/java/com/fujiyama/pulp/developerprofiler/ProfileActivity.java
@@ -3,11 +3,14 @@ package com.fujiyama.pulp.developerprofiler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.fujiyama.pulp.developerprofiler.model.User;
 import com.fujiyama.pulp.developerprofiler.utilities.ImageHandler;
 import com.fujiyama.pulp.developerprofiler.utilities.ImageTransform;
 import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -20,11 +23,25 @@ public class ProfileActivity extends AppCompatActivity {
         User user = (User) userData.getSerializable("user");
 
         ImageView profileImage = (ImageView) findViewById(R.id.profileImage);
-
         Picasso.get()
                 .load(user.getAvatarUrl().toString())
                 .transform(new ImageTransform())
                 .into(profileImage);
+
+        TextView profileName = (TextView) findViewById(R.id.profileName);
+        profileName.setText(user.getFullName());
+
+        TextView profileBio = (TextView) findViewById(R.id.profileBio);
+        profileBio.setText(user.getBio());
+
+        TextView profileRepos = (TextView) findViewById(R.id.profileRepos);
+        profileRepos.setText(user.getPublicRepos().toString());
+
+        TextView profileGists = (TextView) findViewById(R.id.profileGists);
+        profileGists.setText(user.getPublicGists().toString());
+
+        TextView profileFollowers = (TextView) findViewById(R.id.profileFollowers);
+        profileFollowers.setText(user.getFollowers().toString());
 
     }
 }
