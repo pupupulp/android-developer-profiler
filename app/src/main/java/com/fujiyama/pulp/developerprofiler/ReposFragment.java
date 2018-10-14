5a66cba77d7448317e8226973df46e32cd5cd310
Eagan Martin <mece.martinece@gmail.com>
2018-10-14 10:59:15 +0800

    Added fragments and fragments adapter;

--- app/src/main/java/com/fujiyama/pulp/developerprofiler/ReposFragment.java
+++ app/src/main/java/com/fujiyama/pulp/developerprofiler/ReposFragment.java
@@ -1,10 +1,24 @@
 package com.fujiyama.pulp.developerprofiler;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class ReposFragment extends Fragment {
 
+    View view;
     public ReposFragment() {
     }
-    
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_repos, container, false);
+
+        return view;
+    }
 }
---
+++ app/src/main/java/com/fujiyama/pulp/developerprofiler/utilities/FragmentViewPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.fujiyama.pulp.developerprofiler.utilities;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> fragments = new ArrayList<>();
+    private final List<String> fragmentsTitle = new ArrayList<>();
+
+    public FragmentViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentsTitle.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentsTitle.get(position);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragments.add(fragment);
+        fragmentsTitle.add(title);
+    }
+}
