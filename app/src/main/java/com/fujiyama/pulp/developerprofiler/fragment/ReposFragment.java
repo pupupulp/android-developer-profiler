5a4cd9ad9b13fe21080b35a65fd2fa3b66380e44
Eagan Martin <mece.martinece@gmail.com>
2018-10-19 19:26:31 +0800

    Prepared recycler view for repo;

--- app/src/main/java/com/fujiyama/pulp/developerprofiler/fragment/ReposFragment.java
+++ app/src/main/java/com/fujiyama/pulp/developerprofiler/fragment/ReposFragment.java
@@ -4,15 +4,21 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.fujiyama.pulp.developerprofiler.R;
+import com.fujiyama.pulp.developerprofiler.config.DeveloperProfiler;
+import com.fujiyama.pulp.developerprofiler.utility.RepoRecyclerViewAdapter;
 
 public class ReposFragment extends Fragment {
 
     View view;
+    private RecyclerView recyclerView;
+
     public ReposFragment() {
     }
 
@@ -20,6 +26,12 @@ public class ReposFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_repos, container, false);
+
+        RepoRecyclerViewAdapter repoRecyclerViewAdapter = new RepoRecyclerViewAdapter(DeveloperProfiler.getInstance(), DeveloperProfiler.getRepos());
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.repoRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(DeveloperProfiler.getInstance()));
+        recyclerView.setAdapter(repoRecyclerViewAdapter);
 
         return view;
     }
---
+++ app/src/main/java/com/fujiyama/pulp/developerprofiler/utility/RepoRecyclerViewAdapter.java
@@ -0,0 +1,60 @@
+package com.fujiyama.pulp.developerprofiler.utility;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.fujiyama.pulp.developerprofiler.R;
+import com.fujiyama.pulp.developerprofiler.model.Repo;
+
+import java.util.ArrayList;
+
+public class RepoRecyclerViewAdapter extends RecyclerView.Adapter<RepoRecyclerViewAdapter.RepoViewHolder> {
+
+    Context context;
+    ArrayList<Repo> repos;
+
+    public RepoRecyclerViewAdapter(Context context, ArrayList<Repo> repos) {
+        this.context = context;
+        this.repos = repos;
+    }
+
+    @NonNull
+    @Override
+    public RepoViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view;
+        view = LayoutInflater.from(context).inflate(R.layout.item_repo, viewGroup, false);
+
+        RepoViewHolder viewHolder = new RepoViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RepoViewHolder repoViewHolder, int i) {
+        repoViewHolder.repoName.setText(repos.get(i).getFullName());
+        repoViewHolder.repoDescription.setText(repos.get(i).getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return repos.size();
+    }
+
+    public static class RepoViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView repoName;
+        private TextView repoDescription;
+
+        public RepoViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            repoName = (TextView) itemView.findViewById(R.id.repoName);
+            repoDescription = (TextView) itemView.findViewById(R.id.repoDescription);
+        }
+    }
+}
